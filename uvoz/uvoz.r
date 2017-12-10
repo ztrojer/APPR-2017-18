a0c7ce2c8bf3f77598155ced9d81c9c54e65bd6f
Ziga <ziggaaa257@gmail.com>
2017-12-10 20:40:00 +0100

    posodobljen uvoz, dodani podatki

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,69 +1,52 @@
 # 2. faza: Uvoz podatkov
 
-sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
+library(rvest)
+library(gsubfn)
+library(readr)
+library(dplyr)
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
-  return(data)
-}
-
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
-
+#sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
 
 #Funkcija, ki uvozi tabelo 1: Proizvodnja električne energije po proizvodnih delih v EU
-  uvozi.tabela1 <- function () {
-    stolpci1 <- c("LETO", "DRZAVA", "ENOTA", "PRODUKT", "TIP", "VREDNOST", "KOMENTAR")
-    tabela1 <- read_csv("podatki/proizvedena_e_energija/elektrika.csv", locale=locale(encoding="cp1250"),
+uvozi.tabela1 <- function () {
+    stolpci1 <- c("LETO", "DRZAVA", "ENOTA", "PRODUKT", "TIP", "VREDNOST")
+    tabela1 <- read_csv("podatki/proizvedena_e_energija/elektrika_proi.csv", locale=locale(encoding="UTF-8"),
                         col_names= stolpci1,
                         skip = 1,
-                        na = "NA")
-    tabela1$'KOMENTAR' <- NULL
+                        na = c("0",":"))
+    for (i in 1:ncol(tabela1)) {
+      if (is.character(tabela1[[i]])) {
+        Encoding(tabela1[[i]]) <- "UTF-8"
+      }
+    }
     tabela1$'PRODUKT' <- NULL
+    #tabela1$VREDNOST <- parse_integer(tabela1$VREDNOST)
+    #tabela1 <- tabela1[c(2,1,4,3,5)]
+    
     return(tabela1)
   }
   
   tabela1 <- uvozi.tabela1()
+
+
+  
+#Funkcija, ki uvozi tabelo 2: Poraba električne energije po panogi
+uvozi.tabela2 <- function () {
+    stolpci2 <- c("LETO", "DRZAVA", "ENOTA", "PRODUKT","TIP", "VREDNOST")
+    tabela2 <- read_csv("podatki/poraba_e_energija/elektrika_por.csv", locale=locale(encoding="UTF-8"),
+                        col_names= stolpci2,
+                        skip = 1,
+                        na = "0")
+    for (i in 1:ncol(tabela2)) {
+      if (is.character(tabela2[[i]])) {
+        Encoding(tabela2[[i]]) <- "UTF-8"
+      }
+    }
+    #tabela2$VREDNOST <- parse_integer(tabela2$VREDNOST)
+    tabela2$'PRODUKT' <- NULL
+    #tabela2 <- tabela2[c(2,1,4,3,5)]
+    
+    return(tabela2)
+  }
+  
+  tabela2 <- uvozi.tabela2()
