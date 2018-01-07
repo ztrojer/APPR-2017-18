8d59a253dbb007eb4dc837be8ba4fd70c8d6c992
Ziga <ziggaaa257@gmail.com>
2018-01-07 15:56:38 +0100

    zrisan zemljevid - proizvodnja el. energije & delez elektricne enerigje iz obn. virov

--- vizualizacija/zemljevid.R
+++ vizualizacija/zemljevid.R
@@ -5,3 +5,34 @@ evropa <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturaleart
                                   long > -30)
 #zemljevid brez podatkov
 ggplot() + geom_polygon(data = evropa, aes(x = long, y = lat, group = group)) + coord_map(xlim = c(-25, 40), ylim = c(32, 72))
+
+
+#poskrbim, da se imena držav (oziroma po čemer združujem), ujemajo
+evropa1 <- evropa 
+tabela.1$DRZAVA <- gsub("Czech Republic", "Czechia", tabela.1$DRZAVA)
+tabela.1$DRZAVA <- gsub("Bosnia and Herzegovina", "Bosnia and Herz.", tabela.1$DRZAVA)  
+filter2$DRZAVA <- gsub("Czech Republic", "Czechia", filter2$DRZAVA)
+filter2$DRZAVA <- gsub("Bosnia and Herzegovina", "Bosnia and Herz.", filter2$DRZAVA)  
+evropa1$NAME <- gsub("Serbia", "Former Yugoslav", evropa1$NAME)
+evropa1$NAME <- gsub("Bosnia and Herz.", "Former Yugoslav", evropa1$NAME)
+evropa1$NAME <- gsub("Albania", "Former Yugoslav", evropa1$NAME)
+evropa1$NAME <- gsub("Montenegro", "Former Yugoslav", evropa1$NAME)
+evropa1$NAME <- gsub("Macedonia", "Former Yugoslav", evropa1$NAME)
+evropa1$NAME <- gsub("Kosovo", "Former Yugoslav", evropa1$NAME)
+
+
+#narisem zemljevide
+
+#Po proizvodnji električne energije
+zemljevid1 <- ggplot() + geom_polygon(data = left_join(evropa, 
+                                                      tabela.1,
+                                                      by = c("NAME" = "DRZAVA")),
+                                     aes(x = long, y = lat, group = group, fill = SKUPNO)) +
+  coord_map(xlim = c(-25, 40), ylim = c(32, 72))
+
+#Po deležu električne energije iz obnovljivih virov
+zemljevid2 <- ggplot() + geom_polygon(data = left_join(evropa1, 
+                                                       filter2,
+                                                       by = c("NAME" = "DRZAVA")),
+                                      aes(x = long, y = lat, group = group, fill = VREDNOST)) +
+  coord_map(xlim = c(-25, 40), ylim = c(32, 72))
